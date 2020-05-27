ee60f49dc7200a77de7dfd001c1495658d2cc43a
Hacktix <zecmad16@htlkaindorf.at>
2020-05-27 13:50:05 +0200

    Set up project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChromaBoy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
