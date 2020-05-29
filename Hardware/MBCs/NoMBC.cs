582c429a7495da7dffc7979ca21d04a5dffd70b0
Hacktix <zecmad16@htlkaindorf.at>
2020-05-29 20:09:09 +0200

    Implemented "No MBC"-MBC

---
+++ Hardware/MBCs/NoMBC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChromaBoy.Hardware.MBCs
+{
+    class NoMBC : MemoryBankController
+    {
+        public NoMBC(int RAMSize) : base(RAMSize, 0) { }
+
+        public override bool AccessesROM(int address)
+        {
+            return address >= 0 && address < 0x8000;
+        }
+
+        public override bool HandleWrite(int address, byte value)
+        {
+            return true;
+        }
+
+        public override bool IsAddressReadable(int address)
+        {
+            return true;
+        }
+
+        public override bool IsAddressWritable(int address)
+        {
+            return address >= 0x8000 && !(address >= 0xFEA0 && address <= 0xFEFF);
+        }
+
+        public override int TranslateAddress(int address)
+        {
+            if (address >= 0xE000 && address <= 0xFDFF)
+                return address - 0x2000;
+            return address;
+        }
+    }
+}
--- Hardware/MemoryBankController.cs
+++ Hardware/MemoryBankController.cs
@@ -6,6 +6,15 @@ namespace ChromaBoy.Hardware
 {
     public abstract  class MemoryBankController
     {
+        public int RAMSize;
+        public int MemoryBanks;
+
+        public MemoryBankController(int RAMSize, int MemoryBanks)
+        {
+            this.RAMSize = RAMSize;
+            this.MemoryBanks = MemoryBanks;
+        }
+
         public abstract bool IsAddressWritable(int address);
         public abstract bool IsAddressReadable(int address);
         public abstract bool AccessesROM(int address);
