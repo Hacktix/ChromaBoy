75e8b5d2b06363c84a9f9c4c9f41c73fa2b96ee9
Hacktix <zecmad16@htlkaindorf.at>
2020-05-31 16:53:33 +0200

    Added decoding for LD r,n

--- Software/Decoder.cs
+++ Software/Decoder.cs
@@ -9,6 +9,7 @@ namespace ChromaBoy.Software
         public static Opcode DecodeOpcode(Gameboy parent, byte code)
         {
             if ((code & 0b11000000) == 0b01000000) return new LD(parent, code);
+            else if ((code & 0b11000111) == 0b00000110) return new LDI(parent, code);
             throw new NotImplementedException();
         }
     }
