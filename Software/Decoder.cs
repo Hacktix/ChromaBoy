fa0528bbe65550416fb1a96bd1aa1691f673d1bc
Hacktix <zecmad16@htlkaindorf.at>
2020-06-01 16:30:40 +0200

    Implemented OR instruction

--- Software/Decoder.cs
+++ Software/Decoder.cs
@@ -36,6 +36,7 @@ namespace ChromaBoy.Software
             else if ((code & 0b11111111) == 0b11100110) return new ANDI(parent);
             else if ((code & 0b11111000) == 0b10101000) return new XOR(parent, code);
             else if ((code & 0b11111111) == 0b11101110) return new XORI(parent);
+            else if ((code & 0b11111000) == 0b10110000) return new OR(parent, code);
             throw new NotImplementedException();
         }
     }
---
+++ Software/Opcodes/Logic8/OR.cs
@@ -0,0 +1,26 @@
+﻿using ChromaBoy.Hardware;
+
+namespace ChromaBoy.Software.Opcodes
+{
+    public class OR : Opcode // OR r
+    {
+        private Register source;
+
+        public OR(Gameboy parent, byte opcode) : base(parent) {
+            source = OpcodeUtils.BitsToRegister(opcode & 0b111);
+
+            Cycles = source == Register.M ? 8 : 4;
+        }
+
+        public override void Execute()
+        {
+            parent.Registers[Register.A] |= (source == Register.M) ? parent.Memory[(parent.Registers[Register.H] << 8) | (parent.Registers[Register.L])] : parent.Registers[source];
+
+            // Set Flags
+            parent.SetFlag(Flag.Zero, parent.Registers[Register.A] == 0);
+            parent.SetFlag(Flag.AddSub, false);
+            parent.SetFlag(Flag.HalfCarry, false);
+            parent.SetFlag(Flag.Carry, false);
+        }
+    }
+}
