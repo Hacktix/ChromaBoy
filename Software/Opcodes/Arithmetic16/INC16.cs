43a75514fa64ddbfd6827194e79c261bb2a1cefb
Hacktix <zecmad16@htlkaindorf.at>
2020-06-01 17:23:09 +0200

    Implemented 16-bit INC instruction

---
+++ Software/Opcodes/Arithmetic16/INC16.cs
@@ -0,0 +1,18 @@
+﻿using ChromaBoy.Hardware;
+
+namespace ChromaBoy.Software.Opcodes
+{
+    public class INC16 : Opcode // INC rr
+    {
+        public INC16(Gameboy parent, byte opcode) : base(parent) {
+            source = OpcodeUtils.BitsToRegister16((opcode & 0b1110000) >> 4);
+
+            Cycles = 8;
+        }
+
+        public override void Execute()
+        {
+            parent.WriteRegister16(Register16.HL, (ushort)(parent.ReadRegister16(Register16.HL) + 1));
+        }
+    }
+}
