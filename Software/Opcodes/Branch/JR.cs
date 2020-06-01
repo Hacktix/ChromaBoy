dffa1d0dddcc935f472e44aaa6c38039fc7bce31
Hacktix <zecmad16@htlkaindorf.at>
2020-06-01 20:28:02 +0200

    Implemented JR instruction

---
+++ Software/Opcodes/Branch/JR.cs
@@ -0,0 +1,19 @@
+﻿using ChromaBoy.Hardware;
+
+namespace ChromaBoy.Software.Opcodes
+{
+    public class JR : Opcode // JR e
+    {
+        public JR(Gameboy parent) : base(parent) {
+            Length = 2;
+            Cycles = 12;
+        }
+
+        public override void Execute()
+        {
+            byte param = parent.Memory[parent.PC + 1];
+            int addval = (param & 128) > 0 ? -(param & 0x7F) : param & 0x7F;
+            parent.PC = (ushort)(parent.PC + addval);
+        }
+    }
+}
