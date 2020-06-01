ebe6a46d5da4c9d07a032530c0e0cd37fbd46466
Hacktix <zecmad16@htlkaindorf.at>
2020-06-02 00:34:50 +0200

    Implemented BIT instructions

--- Software/Decoder.cs
+++ Software/Decoder.cs
@@ -12,6 +12,7 @@ namespace ChromaBoy.Software
             else if ((code & 0b11110000) == 0b00100000) return new PSA(parent, code);
             else if ((code & 0b11111000) == 0b00110000) return new SWAP(parent, code);
             else if ((code & 0b11111000) == 0b00111000) return new PSL(parent, code);
+            else if ((code & 0b11000000) == 0b01000000) return new BIT(parent, code);
             throw new NotImplementedException();
         }
 
---
+++ Software/Opcodes/BitOps/BIT.cs
@@ -0,0 +1,26 @@
+﻿using ChromaBoy.Hardware;
+
+namespace ChromaBoy.Software.Opcodes
+{
+    public class BIT : Opcode // BIT r
+    {
+        private Register target;
+        private byte bit;
+
+        public BIT(Gameboy parent, byte opcode) : base(parent) {
+            target = OpcodeUtils.BitsToRegister(opcode & 0b111);
+            bit = (byte)((opcode & 0b111000) >> 3);
+
+            Cycles = target == Register.M ? 12 : 8;
+            Length = 2;
+        }
+
+        public override void Execute()
+        {
+            byte tVal = (target == Register.M) ? parent.Memory[(parent.Registers[Register.H] << 8) | (parent.Registers[Register.L])] : parent.Registers[target];
+            parent.SetFlag(Flag.Zero, (tVal & (1 << bit)) == 0);
+            parent.SetFlag(Flag.AddSub, false);
+            parent.SetFlag(Flag.HalfCarry, false);
+        }
+    }
+}
