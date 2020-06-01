e2aff67e040fdb873ce8026ebae303a7423509ee
Hacktix <zecmad16@htlkaindorf.at>
2020-06-01 18:55:42 +0200

    Implemented JP instruction

--- Software/Decoder.cs
+++ Software/Decoder.cs
@@ -52,6 +52,7 @@ namespace ChromaBoy.Software
             else if ((code & 0b11111111) == 0b00000000) return new NOP();
             else if ((code & 0b11111111) == 0b11110011) return new DI(parent);
             else if ((code & 0b11111111) == 0b11111011) return new EI(parent);
+            else if ((code & 0b11111111) == 0b11000011) return new JP(parent);
             throw new NotImplementedException();
         }
     }
---
+++ Software/Opcodes/Branch/JP.cs
@@ -0,0 +1,17 @@
+﻿using ChromaBoy.Hardware;
+
+namespace ChromaBoy.Software.Opcodes
+{
+    public class JP : Opcode // JP nn nn
+    {
+        public JP(Gameboy parent) : base(parent) {
+            Length = 3;
+            Cycles = 16;
+        }
+
+        public override void Execute()
+        {
+            parent.PC = (ushort)(parent.Memory[parent.PC + 1] + (parent.Memory[parent.PC + 2] << 8));
+        }
+    }
+}
