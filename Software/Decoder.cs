0f2df8c10f6847e245fefbf7e215373641ded6e7
Hacktix <zecmad16@htlkaindorf.at>
2020-06-01 16:35:28 +0200

    Implemented ORI instruction

--- Software/Decoder.cs
+++ Software/Decoder.cs
@@ -37,6 +37,7 @@ namespace ChromaBoy.Software
             else if ((code & 0b11111000) == 0b10101000) return new XOR(parent, code);
             else if ((code & 0b11111111) == 0b11101110) return new XORI(parent);
             else if ((code & 0b11111000) == 0b10110000) return new OR(parent, code);
+            else if ((code & 0b11111111) == 0b11110110) return new ORI(parent);
             throw new NotImplementedException();
         }
     }
