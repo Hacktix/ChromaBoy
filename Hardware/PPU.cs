f0cb8bb5a19872a49830eb39ffdf113b38f81c23
Hacktix <zecmad16@htlkaindorf.at>
2020-06-06 19:56:51 +0200

    Implemented scanline-based Background Scrolling

--- Hardware/PPU.cs
+++ Hardware/PPU.cs
@@ -97,12 +97,9 @@ namespace ChromaBoy.Hardware
                 byte color = (byte)(lc | uc);
                 byte shade = color == 0 ? (byte)(parent.Memory.Get(0xFF47) & 0b11) : color == 1 ? (byte)((parent.Memory.Get(0xFF47) & 0b1100) >> 2) : color == 2 ? (byte)((parent.Memory.Get(0xFF47) & 0b110000) >> 4) : (byte)((parent.Memory.Get(0xFF47) & 0b11000000) >> 6);
 
-                Background[bSLX, ly] = shade;
+                Background[(byte)(bSLX - parent.Memory.Get(0xFF43)), (byte)(ly - parent.Memory.Get(0xFF42))] = shade;
 
                 if (++bSLX == 0) LineDone = true;
-
-                BGScrX = parent.Memory.Get(0xFF43);
-                BGScrY = parent.Memory.Get(0xFF42);
             }
 
             // Increment Cycle Count
@@ -116,7 +113,7 @@ namespace ChromaBoy.Hardware
             {
                 for(int y = 0; y < Emulator.SCREEN_HEIGHT; y++)
                 {
-                    Display[x, y] = Background[(byte)(BGScrX + x), (byte)(BGScrY + y)];
+                    Display[x, y] = Background[x, y];
                 }
             }
         }
