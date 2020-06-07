9a2b071910a9e887f3aa41f799440fdd99d02e46
Hacktix <zecmad16@htlkaindorf.at>
2020-06-07 17:25:03 +0200

    Implemented cycle-accurate memory access for LDAI instructions

--- Software/Opcodes/Loads/LDAI.cs
+++ Software/Opcodes/Loads/LDAI.cs
@@ -5,21 +5,29 @@ namespace ChromaBoy.Software.Opcodes
     public class LDAI : Opcode // LD A, (nn) | LD (nn), A
     {
         private bool load;
+        private int actionTick;
 
         public LDAI(Gameboy parent, byte opcode) : base(parent) {
             load = (opcode & 0b10000) > 0;
 
             Cycles = 16;
             Length = 3;
+
+            TickAccurate = true;
+            actionTick = 8;
         }
 
-        public override void Execute()
+        public override void ExecuteTick()
         {
-            byte srcVal = load ? parent.Memory[parent.Memory[parent.PC + 1] + (parent.Memory[parent.PC + 2] << 8)] : parent.Registers[Register.A];
-            if (load)
-                parent.Registers[Register.A] = srcVal;
-            else
-                parent.Memory[parent.Memory[parent.PC + 1] + (parent.Memory[parent.PC + 2] << 8)] = srcVal;
+            base.ExecuteTick();
+            if (Tick == actionTick)
+            {
+                byte srcVal = load ? parent.Memory[parent.Memory[parent.PC + 1] + (parent.Memory[parent.PC + 2] << 8)] : parent.Registers[Register.A];
+                if (load)
+                    parent.Registers[Register.A] = srcVal;
+                else
+                    parent.Memory[parent.Memory[parent.PC + 1] + (parent.Memory[parent.PC + 2] << 8)] = srcVal;
+            }
         }
     }
 }
