2dbbbbea6ae89b615ef8d51c45738390de7b5de1
Hacktix <zecmad16@htlkaindorf.at>
2020-06-07 17:17:50 +0200

    Implemented cycle-accurate memory access for LDI instructions

--- Software/Opcodes/Loads/LDI.cs
+++ Software/Opcodes/Loads/LDI.cs
@@ -5,21 +5,29 @@ namespace ChromaBoy.Software.Opcodes
     public class LDI : Opcode // LD r, n
     {
         private Register target;
+        private int actionTick;
 
         public LDI(Gameboy parent, byte opcode) : base(parent) {
             target = OpcodeUtils.BitsToRegister((opcode & 0b111000) >> 3);
 
             Cycles = target == Register.M ? 12 : 8;
             Length = 2;
+
+            TickAccurate = true;
+            actionTick = target == Register.M ? 4 : 2;
         }
 
-        public override void Execute()
+        public override void ExecuteTick()
         {
-            byte srcVal = parent.Memory[parent.PC + 1];
-            if (target == Register.M)
-                parent.Memory[(parent.Registers[Register.H] << 8) | (parent.Registers[Register.L])] = srcVal;
-            else
-                parent.Registers[target] = srcVal;
+            base.ExecuteTick();
+            if(Tick == actionTick)
+            {
+                byte srcVal = parent.Memory[parent.PC + 1];
+                if (target == Register.M)
+                    parent.Memory[(parent.Registers[Register.H] << 8) | (parent.Registers[Register.L])] = srcVal;
+                else
+                    parent.Registers[target] = srcVal;
+            }
         }
     }
 }
