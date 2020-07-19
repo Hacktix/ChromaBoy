660ea94d509c4dac96dd15f5fd2905fc5fcf5e67
Hacktix <zecmad16@htlkaindorf.at>
2020-07-19 17:53:47 +0200

    Fixed DIV memory reads

--- Hardware/Memory.cs
+++ Hardware/Memory.cs
@@ -89,7 +89,7 @@ namespace ChromaBoy.Hardware
                 {
                     case 0xFF02: return (byte)(RAM[0xFF02] | 0b1111110);
                     case 0xFF03: return 0xFF;
-                    case 0xFF04: return (byte)((parent.DivRegister & 0xFF) >> 8);
+                    case 0xFF04: return (byte)((parent.DivRegister & 0xFF00) >> 8);
                     case 0xFF07: return (byte)(RAM[0xFF07] | 0b11111000);
                     case 0xFF08:
                     case 0xFF09:
